20fbc8ccc4dcbd2fd83d041f48b26ab7dbee1659
olivejua <tmfrl4710@gmail.com>
2021-06-03 20:47:09 +0900

    Initial commit

---
+++ src/main/java/com/olivejua/practicespringsecurity/PracticeSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.olivejua.practicespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olivejua/practicespringsecurity/PracticeSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olivejua.practicespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
